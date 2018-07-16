7d47875e04f9d0b9e742300129729b5dfcf8f6b3
Victor Ordu <victorordu@outlook.com>
2018-07-16 15:50:58 +0100

    Improve unit tests

--- tests/testthat/test-build_report.R
+++ tests/testthat/test-build_report.R
@@ -5,10 +5,9 @@ context("Report generation")
 test_that("appropriate data are sourced", {
   expect_error(build_webreport())
   expect_error(build_webreport(), "'data.source' must be provided.")
-  expect_error(build_webreport(data.source = "file.txt"))
-  expect_error(build_webreport(data.source = "no-extension"))
-  # expect_error(build_webreport("fakeFile.Rmd", "fakeDB.db", gui = FALSE),
-  #              "file 'fake_path' does not exist")
+  expect_error(build_webreport(data.source = "nofile.txt"), 'No file')
+  expect_error(build_webreport(data.source = "test-data/ntweets.rds"),
+               '\'data.source\' should be an SQLite database')
 })
 
 test_that("altered fields from database are reprocessed", {
