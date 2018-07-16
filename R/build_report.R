ae3fe65339a0ce40bb0528b69e1d2d74e1959096
Victor Ordu <victorordu@outlook.com>
2018-07-16 14:10:32 +0100

    Added binary check for SQLite file format

--- R/build_report.R
+++ R/build_report.R
@@ -45,37 +45,44 @@ build_webreport <- function(data.source, outfile = NULL, launch.file = TRUE, ...
     if (!file.exists(data.source))
       stop("No file", sQuote(data.source))
   }
-  if (!grepl('.db$|.sqlite$', data.source, ignore.case = TRUE)) {
-    stop("The file 'data.source' should have '.db' or '.sqlite' extension.")
-    # TODO: Use binary file validation
-  }
 
-  ## Validation or creation of output filename
-  fext <- ".docx"
+  file.con <- file(data.source, 'rb')
+  header <- readBin(file.con, character(), endian = 'big', size = 16)
+  on.exit(close(file.con))
+  if (header != 'SQLite format 3')
+    stop("'data.source' should be an SQLite database.")
+
+  ## Validation/creation of output filepath
+  ms.word <- ".docx"
   if (is.null(outfile)) {
     outfile <-
     tempfile(paste0("report_", format(Sys.time(), '%Y%m%d_%H%M%S')),
-             tmpdir = ".", fileext = fext)
+             tmpdir = ".", fileext = ms.word)
   }
   else {
     outfile <- as.character(outfile)
-    if (!endsWith(tolower(outfile), fext))
-      outfile <- paste0(outfile, fext)
+    if (!endsWith(tolower(outfile), ms.word))
+      outfile <- paste0(outfile, ms.word)
 
       ## Check for...
-      anyDuplicDots <- "\\.{2,}"
-      if (grepl(anyDuplicDots, outfile))
-        outfile <- gsub(anyDuplicDots, ".", outfile)
+      dupl.dots <- "\\.{2,}"
+      if (grepl(dupl.dots, outfile))
+        outfile <- gsub(dupl.dots, ".", outfile)
   }
-  template <- system.file("rmarkdown/templates/reports/skeleton/skeleton.Rmd",
-                     package = "webreport",
-                     mustWork = TRUE)
+  template <-
+    system.file(
+      "rmarkdown/templates/reports/skeleton/skeleton.Rmd",
+      package = "webreport",
+      mustWork = TRUE
+    )
   data <- provide_internal_data(data.source)
-  rmarkdown::render(input = template,
-                    output_format = "word_document",
-                    output_file = outfile,
-                    output_dir = dirname(outfile),
-                    params = list(data = data))
+  rmarkdown::render(
+    input = template,
+    output_format = "word_document",
+    output_file = outfile,
+    output_dir = dirname(outfile),
+    params = list(data = data)
+  )
   if (launch.file)
     try(system2("open", args = outfile, invisible = FALSE))
   message(paste("Report has been saved to",
