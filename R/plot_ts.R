10ea53766abb68bac6933a06ce1e0f8fb7d01e6c
Victor Ordu <victorordu@outlook.com>
2018-09-19 15:15:51 +0100

    Fix time-series plot labels and add legend

--- R/plot_ts.R
+++ R/plot_ts.R
@@ -9,54 +9,61 @@ globalVariables(c('.', 'week'))
 #' @param data An object of class \code{data.frame}
 #' @param platform A character string, with the name of the chosein online
 #' platform. Options are Facebook, Twitter and Website.
-#' @param base Logical; whether to create a base plot or a ggplot.
-#' @param pt An integer value represeting the weight of the line. Defaults to 2L.
+#' @param ... Additional arguments passed to \code{plot}.
 #'
-#' @details The data used for this function i.e. the date variable, is expected
-#' to be either a nummeric vector (as stored by Twitter and Website data) or as
-#' a character vector (Facebook). The Facebook date elements are stored in the
-#' POSIX-compatible \code{YYYY-MM-DDTHH:MM:SS+} format. Any other presentation
-#' produce an error.
 #'
 #' @return Has no return value per se. It results in the production of a
-#' time-series plot (either a base \code{graphics} or a
-#' \code{\link[ggplot2]{ggplot}} type of plot.
+#' time-series plot.
 #'
-#' @import dplyr
-#' @import ggplot2
-#' @importFrom graphics plot
 #' @importFrom stats ts
 #'
 #' @export
-make_ts <- function(data, platform, base = TRUE, pt = 2L) {
+make_ts <- function(data, platform, ...) {
   stopifnot(inherits(data, 'data.frame'))
   stopifnot(is.character(platform))
   ps <- platformSpecs(data, platform)
   res <- prepare(ps,'NESREANigeria')
-  tsObj <-
-    ts(
-      data = res,
-      start = c(2016, 12),
-      end = c(2018, 8),
-      frequency = 12
-    )
+  tso <- ts(data = res,
+            start = c(2016, 12),    # TODO: Change hard coding here!
+            end = c(2018, 8),
+            frequency = 12)
+  .drawTimeSeries(tso, specs = ps)
+}
 
+
+
+#' @importFrom graphics plot
+#' @importFrom stats window
+.drawTimeSeries <- function(tsObj, startDate = -365L, base = TRUE, specs, ...)
+{
+  stopifnot(exprs = {
+    inherits(tsObj, 'ts')
+    is.numeric(startDate)
+    is.logical(base)
+    inherits(specs, 'platformSpecs')
+  })
   if (base) {
-    startDate <- -365
+    colour <- specs$colour
+    legend <- 'All updates'
+    if(is.matrix(tsObj) && ncol(tsObj) == 2) {
+      colour <- c(colour, 'red')
+      legend <- c(legend, 'NESREA')
+    }
     plot(
       window(
         tsObj,
-        start = c(
-          .numericalDateElem('%Y', startDate),
-          .numericalDateElem('%m', startDate)
-        ),
-        end = c(.numericalDateElem('%Y'), .numericalDateElem('%m'))
+        start = c(.numericalDateElem('%Y', startDate),
+                  .numericalDateElem('%m', startDate)),
+        end = c(.numericalDateElem('%Y'),
+                .numericalDateElem('%m'))
       ),
       plot.type = 'single',
-      col = c(ps$colour, 'red'),
+      col = colour,
       lwd = 2,
-      main = ps$title.stub
+      main = specs$title.stub,
+      ylab = 'Posts'
     )
+    legend('topright', legend = legend, fill = colour)
   }
   else {
     message("Other plotting formats not yet implemented")
@@ -71,17 +78,15 @@ make_ts <- function(data, platform, base = TRUE, pt = 2L) {
 
 
 
-
-
-
-
-
-## Gets the numerical value for a given element of a Date object
+## Returns the numerical value for a given element of a Date object
 .numericalDateElem <- function(placeholder, dif = 0) {
-  stopifnot(is.character(placeholder), grepl('^%[A-Za-z]{1}', placeholder))
+  stopifnot(exprs = {
+    is.character(placeholder)
+    grepl('^%[A-Za-z]{1}', placeholder)
+  })
   if (!is.numeric(dif) & !is.integer(dif))
     stop("'dif' should be a number")
   if (abs(dif) > 365)
-    warning('You have elected to pick a time frame larger than 1 year')
+    warning('You have elected to use a time frame larger than 1 year')
   as.numeric(format(Sys.Date() + dif, placeholder))
 }
