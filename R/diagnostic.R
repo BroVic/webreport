9cc2412495bdb4590dbd1585e4355dce437bb513
Victor Ordu <victorordu@outlook.com>
2018-10-16 13:58:53 +0100

    Update diagnostic functions

--- R/diagnostic.R
+++ R/diagnostic.R
@@ -1,10 +1,23 @@
 # diagnostic.R
 
+## The functions in this file are basically meant to ease in-project analyses
+## e.g. loading of the data. This file is not part of the final build i.e.
+## it will be added to '.Rbuildignore'.
+
+
 ## This operation is abstracted to avoid repetition. The path provided
 ## as an argument is specific to a particular computer
+if (Sys.info()['nodename'] %in% 'SA-DG') {
+  .dataB <- '../NESREA_social/data/nesreanigeria.db'
+  if (!file.exists(.dataB))
+    warning('The database', sQuote(basename(.dataB)), "was not found")
+}
+
+## Reads all the data
 dat <- function()
 {
-  provideInternalData('../NESREA_social/data/nesreanigeria.db')
+  stopifnot(exists('.dataB', env = parent.env(environment())))
+  provideInternalData(.dataB)
 }
 
 
